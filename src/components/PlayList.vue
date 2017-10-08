cd5008de844af226a8b61d11e6e7d849476fc397
jpgilchrist <james@veoci.com>
2017-10-08 14:36:05 -0400

    add a sort direction and include sort direction column icons

--- src/components/PlayList.vue
+++ src/components/PlayList.vue
@@ -3,13 +3,13 @@
     <div class="flex-header">
       <div class="flex-row">
         <div class="flex-cell" v-for="column in columns">
-          {{column.display}}
+          <span>{{column.display}}</span><icon class="sort-direction" name="caret-down"></icon>
         </div>
         <div class="flex-cell scrollbar-placeholder"></div>
       </div>
     </div>
     <div class="flex-body">
-      <div class="flex-row" v-for="song in playlist">
+      <div class="flex-row" v-for="song in sortedPlaylist">
         <div class="flex-cell" v-for="column in columns">
           {{song[column.value]}}
         </div>
@@ -20,9 +20,16 @@
 
 <script>
   import axios from 'axios'
+  import _ from 'lodash'
+  import 'vue-awesome/icons/caret-down'
+  import 'vue-awesome/icons/caret-up'
+  import Icon from 'vue-awesome/components/Icon.vue'
 
   export default {
     name: 'playList',
+    components: {
+      Icon
+    },
     data () {
       return {
         playlist: null,
@@ -33,11 +40,14 @@
         }, {
           value: 'name',
           display: 'Name'
-        }]
+        }],
+        sort: {
+          arts: 'asc',
+          name: 'asc'
+        }
       }
     },
     created: function () {
-      console.log('PlayList created')
       axios.get('/api/playlist')
         .then((response) => {
           this.playlist = response.data
@@ -45,6 +55,11 @@
         .catch((error) => {
           this.error = error
         })
+    },
+    computed: {
+      sortedPlaylist () {
+        return _.orderBy(this.playlist, ['arts', 'name'], [this.sort.arts, this.sort.name])
+      }
     }
   }
 </script>
@@ -52,6 +67,7 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
   @borderColor: gray;
+  @headerHoverColor: #ddd;
   .flex-table {
     display: flex;
     flex: 1;
@@ -64,10 +80,24 @@
         border-bottom: 2px solid @borderColor;
         .flex-cell {
           font-weight: bold;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+          cursor: pointer;
+
           &.scrollbar-placeholder {
             width: 14px;
             flex: 0 0 auto;
             background-color: @borderColor;
+          }
+
+          &:hover {
+            background-color: @headerHoverColor;
+          }
+
+          .sort-direction {
+
           }
         }
       }
