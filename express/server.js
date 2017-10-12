c7c3bfe52bd0a0d86635e4f7a8a424441f6a4f4e
Drew Mazurek <dmazurek@users.noreply.github.com>
2017-10-12 08:53:19 -0400

    fixes #16: return only title and artist, strip out duplicates (#17)
    * #16: return only title and artist, strip out duplicates
    
    * #16: missing close paren on a comment

--- express/server.js
+++ express/server.js
@@ -5,7 +5,19 @@ const fs = require('fs')
 const path = require('path')
 
 const library = plist.parse(fs.readFileSync(path.join(__dirname, '../Library.xml'), 'utf8'))
-const playlist = library.Playlists[0]['Playlist Items']
+let dupes = {}
+const playlist = library.Playlists[0]['Playlist Items'].map(function (song) {
+  return {'name': song.name, 'arts': song.arts}
+}).filter(function (song) {
+  // create a key of just alphanumerics (e.g. Joel, Billy - Keeping The Faith => joelbillykeepingthefaith)
+  let key = song.arts.toLowerCase().replace(/[^\w]|_/g, '') + song.name.toLowerCase().replace(/[^\w]|_/g, '')
+  if (dupes[key] === 1) {
+    return false
+  } else {
+    dupes[key] = 1
+    return true
+  }
+})
 
 app.use(function (req, res, next) {
   console.log('Request URL!: ', req.originalUrl)
